08e0ad2d9fa0f0434eaf107077eb8180c8063177
Bart <bart.denboef@gmail.com>
2023-07-02 13:23:11 +0200

    saved output

--- run_model.sh
+++ run_model.sh
@@ -9,10 +9,10 @@
 # Run the model
 
 # Earthy sweep
-python3 train.py --train-batch-size 32 --eval-batch-size 8 --epochs 40 --weight-decay 0 --warmup-steps 15 --logging-steps 10 --learning-rate "3e-4"
+# python3 train.py --train-batch-size 32 --eval-batch-size 8 --epochs 40 --weight-decay 0 --warmup-steps 15 --logging-steps 10 --learning-rate "3e-4"
 
 # Swept sweep
-# python3 train.py --train-batch-size 4 --eval-batch-size 32 --epochs 50 --weight-decay 0 --warmup-steps 5 --logging-steps 10 --learning-rate "8e-5"
+python3 train.py --train-batch-size 4 --eval-batch-size 32 --epochs 50 --weight-decay 0 --warmup-steps 5 --logging-steps 10 --learning-rate "8e-5"
 
 # Ruby sweep
 # python3 train.py --train-batch-size 4 --eval-batch-size 16 --epochs 50 --weight-decay 0 --warmup-steps 10 --logging-steps 10 --learning-rate "6e-5"
